9c89ea48b5687d65877ca69cb2add053e293db2b
John G Mac2 <johngrantuk@googlemail.com>
2019-11-01 20:59:10 +0000

    Updates

---
+++ precache-manifest.22c2027a8a1fc48a923550690b6e236b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "fa67a8b2b3e07667acb1",
+    "url": "/my-app/static/js/main.fa67a8b2.chunk.js"
+  },
+  {
+    "revision": "de0d2a65cc0d4e66f4d2",
+    "url": "/my-app/static/js/1.de0d2a65.chunk.js"
+  },
+  {
+    "revision": "fa67a8b2b3e07667acb1",
+    "url": "/my-app/static/css/main.dc65bfa9.chunk.css"
+  },
+  {
+    "revision": "babf37e7619ce63c8674a9e54697acfa",
+    "url": "/my-app/index.html"
+  }
+];
