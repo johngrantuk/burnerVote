cb3b882dafc128c99370d3f6bf0c1c169b4ba121
John G Mac2 <johngrantuk@googlemail.com>
2019-11-15 13:43:01 +0000

    Tidying.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./utils/getWeb3";
 import Proposals from './components/Proposals';
 import Deposits from './components/Deposits';
-
-// import "./App.css";
 import "./black-dashboard/assets/css/black-dashboard-react.css";
 import "./black-dashboard/assets/css/nucleo-icons.css";
 import AdminNavbar from "./black-dashboard/components/Navbars/AdminNavbar.jsx";
@@ -35,28 +31,11 @@ class App extends Component {
 
   componentDidMount = async () => {
     try {
-      console.log('DidMount...');
-
       console.log('Loading graph...');
       await this.graph();
-      /*
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      */
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      // this.setState({ web3, accounts });
     } catch (error) {
-      // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        'Error loading The Graph',
       );
       console.error(error);
     }
@@ -111,14 +90,6 @@ class App extends Component {
   }
 
   render() {
-
-    if(this.state.contractError){
-      // return <Alert color="danger">No Contract Deployed On Your Current Network - Try Rinkeby?</Alert>;
-    }
-
-    if (!this.state.web3) {
-      // return <div>Loading Web3, accounts, and contract...</div>;
-    }
 
     return (
       <div className="App">
